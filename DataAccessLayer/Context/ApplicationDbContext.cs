596606dd4d30394e1bad7c7a78256a17a825094b
Sayan Pramanik <41745417+sayanpr8175@users.noreply.github.com>
2026-08-09 16:51:54 -0400

    Added Application db context and added nuget packages related to mysql

---
+++ DataAccessLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options)
+    {
+
+    }
+    public DbSet<Product> Products {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ DataAccessLayer/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities;
+
+public class Product
+{
+    [Key]
+    public Guid ProductID { get; set; }
+    public string ProductName { get; set; }
+    public string Category { get; set; }
+    public double? UnitPrice { get; set; }
+    public int? QuantityInStock { get; set; }
+}
