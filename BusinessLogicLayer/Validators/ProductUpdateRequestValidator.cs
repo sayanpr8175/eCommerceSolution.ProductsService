24005c31e5ea3d445bfca61e9ea1c8e6facfa64a
Sayan Pramanik <41745417+sayanpr8175@users.noreply.github.com>
2026-08-12 15:01:39 -0400

    Added Fluent validations for Add request and update request

---
+++ BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators;
+
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+{
+    public ProductUpdateRequestValidator()
+    {
+        RuleFor(temp => temp.ProductID).NotEmpty().WithMessage("Product Id can not be blank");
+
+        RuleFor(temp => temp.ProductName).NotEmpty().WithMessage("Product Name can not be blank");
+
+        RuleFor(temp => temp.Category).IsInEnum().WithMessage("Select an proper option");
+
+        RuleFor(temp => temp.UnitPrice).InclusiveBetween(0, double.MaxValue)
+            .WithMessage($"Price should be between 0 and {double.MaxValue}");
+
+        RuleFor(temp => temp.QuantityInStock).InclusiveBetween(0, int.MaxValue)
+            .WithMessage($"Quantity should be between 0 and {int.MaxValue}");
+
+    }
+}
